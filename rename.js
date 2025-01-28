7dc65d2200a4dde2323279e5426ef22a0ad8cd25
enlian <frejaenlian@gmail.com>
2025-01-28 21:43:26 +0900

    fix bug

--- rename.js
+++ rename.js
@@ -4,24 +4,36 @@ const path = require('path');
 function cleanMovieTitle(filename) {
     const invalidPatterns = [
         /\b(The|A|An)\.(?=[A-Z])/gi, // 替换 "The." -> "The "
-        /\.(1080p|720p|2160p|4K|8K|HD|WEBRip|WEB-DL|BluRay|BDRip|H\.264|H\.265|x264|x265|HDR|SDR|HEVC|DDP[\.\d]*|AAC|DTS|DTS-HD|TrueHD|Atmos|MA|AC3|Dual|CHS|ENG|GERMAN|HINDI|MULTi|JAPANESE|KOREAN|RARBG|YTS|AMZN|NF|AppleTV|B4E|ETHEL|TUDHER|BDE4|WEB|BD|PROPER|REPACK|IMAX|UNCUT|EXTENDED|Remux|HDTV|CAM|TS|SCR|DVDSCR|DVDRip|TC|HDTS|HDTC)(?=\.|$)/gi, '' // 删除无意义标记
+        /[\s._-]+(1080p|720p|LAMA|NaNi|10bit|2160p|4K|8K|HD|WEBRip|WEB-DL|BluRay|BDRip|H\.264|H\.265|x264|x265|HDR|SDR|HEVC|DDP[\.\d]*|AAC|DTS|DTS-HD|TrueHD|Atmos|MA|AC3|Dual|CHS|ENG|GERMAN|HINDI|MULTi|JAPANESE|KOREAN|RARBG|YTS|AMZN|NF|AppleTV|B4E|ETHEL|TUDHER|BDE4|WEB|BD|PROPER|REPACK|IMAX|UNCUT|EXTENDED|Remux|HDTV|CAM|TS|SCR|DVDSCR|DVDRip|TC|HDTS|HDTC)(?=[\s._-]|$)/gi, '' // 删除无意义标记
     ];
 
-    const removeChineseWords = [
+    const removeWords = [
         '特效', '中英字幕', '中字', '国语', '粤语', '配音', '双字', '高清', '完整版', '特别版', '修复版', 
-        '官方字幕', '电影版', '剧场版', 'TV版', 'BD', 'DVD', '4K修复', 'HDR版'
+        '官方字幕', '电影版', '剧场版', 'TV版', 'BD', 'DVD', '4K修复', 'HDR版', '蓝光', '蓝光版', '蓝光修复',
+        '中英双字', '中英双字幕', '中英字幕', '中英双语', '中英双语字幕', '中英双语字幕版', '中英双语版', '中英双语修复版', '中英双语修复', '中英'
     ];
+
+    const specialCharacters = ['-', '_', '/', ':', '：']; // 特殊字符数组，可扩展
 
     let cleanedName = filename;
+
+    // 删除无意义标记
     invalidPatterns.forEach(pattern => {
         cleanedName = cleanedName.replace(pattern, '');
     });
 
-    removeChineseWords.forEach(word => {
-        cleanedName = cleanedName.replace(new RegExp(`(?:^|\s)${word}(?:\s|$)`, 'gi'), ' ').trim();
+    // 删除无意义中文
+    removeWords.forEach(word => {
+        cleanedName = cleanedName.replace(new RegExp(`(?:^|\s|[-_/])${word}(?:\s|[-_/]|$)`, 'gi'), '');
+    });
+
+    // 删除特殊字符
+    specialCharacters.forEach(char => {
+        cleanedName = cleanedName.replace(new RegExp(`\${char}`, 'g'), ' ');
     });
 
     cleanedName = cleanedName.replace(/(?<!\.)\.(?!\w{2,4}$)/g, ' '); // 替换所有点为空格，但保留扩展名前的点
+    cleanedName = cleanedName.replace(/\s+([\p{Script=Han}])/gu, '$1'); // 修复删除汉字后前面多出的空格
     cleanedName = cleanedName.replace(/\.\s/g, ' '); // 删除点后多余的空格
     cleanedName = cleanedName.replace(/\.{2,}/g, '.'); // 合并多余的点
     cleanedName = cleanedName.replace(/\s{2,}/g, ' ').trim(); // 合并多余空格
@@ -38,7 +50,7 @@ function cleanMovieTitle(filename) {
 function renameFiles(directory) {
     const logEntries = [];
     const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-    const logFile = `log-${timestamp}.txt`;
+    const logFile = `rename-log-${timestamp}.txt`;
 
     fs.readdir(directory, (err, files) => {
         if (err) {
@@ -93,5 +105,5 @@ function renameFiles(directory) {
     });
 }
 
-const targetDirectory = 'E:/影视';
+const targetDirectory = 'D:/下载';
 renameFiles(targetDirectory);
